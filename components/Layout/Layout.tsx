7cff938bb8c4acaf7d6639ae11472950db0b1a0a
Taehyeungkim98 <kth1837@snu.ac.kr>
2023-06-29 07:15:11 +0000

    :sparkles: started creating login page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,21 +2,28 @@ import { ReactNode } from 'react'
 import Header from '@/components/Layout/Header/Header'
 import Main from '@/components/Layout/Main/Main'
 import Footer from '@/components/Layout/Footer/Footer'
+import {useRouter} from 'next/router'
+import styles from './Layout.module.scss';
 
 interface LayoutProps {
     children: ReactNode
 }
 
 export default function Layout({children}:LayoutProps) {
+
+    const router = useRouter();
+
+
+
     return (
-        <>
-        <Header></Header>
+        <div className = {styles.layout}>
+        {router.pathname !== '/login' ? <Header></Header> : null}
         <Main>
             {children}
         </Main>
         <Footer></Footer>
         
-        </>
+        </div>
         
         
     )
